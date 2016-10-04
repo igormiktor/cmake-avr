c2cb5b5fabbfa59f8f62e7523d15e69dcb6bad42
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2016-10-03 22:20:07 -0400

    Modified to eliminate fuse-related things and move more to the toolchain file.  Add a different size display.

--- generic-gcc-avr.cmake
+++ generic-gcc-avr.cmake
@@ -7,6 +7,9 @@
 # return. (I don't like beer much.)
 #
 # Matthias Kleemann
+#
+# Modfied by Igor Mikolic-Torreira
+#
 ##########################################################################
 
 ##########################################################################
@@ -14,14 +17,8 @@
 #
 # AVR_MCU (default: atmega8)
 #     the type of AVR the application is built for
-# AVR_L_FUSE (NO DEFAULT)
-#     the LOW fuse value for the MCU used
-# AVR_H_FUSE (NO DEFAULT)
-#     the HIGH fuse value for the MCU used
 # AVR_UPLOADTOOL (default: avrdude)
 #     the application used to upload to the MCU
-#     NOTE: The toolchain is currently quite specific about
-#           the commands used, so it needs tweaking.
 # AVR_UPLOADTOOL_PORT (default: usb)
 #     the port used for the upload tool, e.g. usb
 # AVR_PROGRAMMER (default: avrispmkII)
@@ -31,26 +28,62 @@
 ##########################################################################
 # options
 ##########################################################################
-option(WITH_MCU "Add the mCU type to the target file name." ON)
+option( WITH_MCU "Add the MCU type to the target file name." OFF )
+
 
 ##########################################################################
 # executables in use
 ##########################################################################
-find_program(AVR_CC avr-gcc)
-find_program(AVR_CXX avr-g++)
-find_program(AVR_OBJCOPY avr-objcopy)
-find_program(AVR_SIZE_TOOL avr-size)
-find_program(AVR_OBJDUMP avr-objdump)
+find_program( AVR_CC avr-gcc )
+find_program( AVR_CXX avr-g++ )
+find_program( AVR_OBJCOPY avr-objcopy )
+find_program( AVR_SIZE_TOOL avr-size )
+find_program( AVR_OBJDUMP avr-objdump )
+find_program( AWK awk )
+
 
 ##########################################################################
 # toolchain starts with defining mandatory variables
 ##########################################################################
-set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_SYSTEM_PROCESSOR avr)
-set(CMAKE_C_COMPILER ${AVR_CC})
-set(CMAKE_CXX_COMPILER ${AVR_CXX})
+set( CMAKE_SYSTEM_NAME Generic )
+set( CMAKE_SYSTEM_PROCESSOR avr )
+set( CMAKE_C_COMPILER ${AVR_CC} )
+set( CMAKE_CXX_COMPILER ${AVR_CXX} )
+
+
+###########################################################################
+# some cmake cross-compile necessities
+##########################################################################
+if( DEFINED ENV{AVR_FIND_ROOT_PATH} )
+    set( CMAKE_FIND_ROOT_PATH $ENV{AVR_FIND_ROOT_PATH} )
+else( DEFINED ENV{AVR_FIND_ROOT_PATH} )
+    if( EXISTS "/opt/local/avr" )
+      set( CMAKE_FIND_ROOT_PATH "/opt/local/avr" )
+    elseif( EXISTS "/usr/avr" )
+      set( CMAKE_FIND_ROOT_PATH "/usr/avr" )
+    elseif( EXISTS "/usr/lib/avr" )
+      set( CMAKE_FIND_ROOT_PATH "/usr/lib/avr" )
+    else( EXISTS "/opt/local/avr" )
+      message( FATAL_ERROR "Please set AVR_FIND_ROOT_PATH in your environment." )
+    endif( EXISTS "/opt/local/avr" )
+endif( DEFINED ENV{AVR_FIND_ROOT_PATH} )
+set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
+set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
+set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
+# not added automatically, since CMAKE_SYSTEM_NAME is "generic"
+set( CMAKE_SYSTEM_INCLUDE_PATH "${CMAKE_FIND_ROOT_PATH}/include" )
+set( CMAKE_SYSTEM_LIBRARY_PATH "${CMAKE_FIND_ROOT_PATH}/lib" )
+
 
 ##########################################################################
+# status messages for generating
+##########################################################################
+message( STATUS "Set CMAKE_FIND_ROOT_PATH to ${CMAKE_FIND_ROOT_PATH}" )
+message( STATUS "Set CMAKE_SYSTEM_INCLUDE_PATH to ${CMAKE_SYSTEM_INCLUDE_PATH}" )
+message( STATUS "Set CMAKE_SYSTEM_LIBRARY_PATH to ${CMAKE_SYSTEM_LIBRARY_PATH}" )
+
+
+#########################################################################
 # some necessary tools and variables for AVR builds, which may not
 # defined yet
 # - AVR_UPLOADTOOL
@@ -61,80 +94,141 @@ set(CMAKE_CXX_COMPILER ${AVR_CXX})
 ##########################################################################
 
 # default upload tool
-if(NOT AVR_UPLOADTOOL)
+if( NOT AVR_UPLOADTOOL )
    set(
       AVR_UPLOADTOOL avrdude
       CACHE STRING "Set default upload tool: avrdude"
    )
-   find_program(AVR_UPLOADTOOL avrdude)
-endif(NOT AVR_UPLOADTOOL)
+   find_program( AVR_UPLOADTOOL avrdude )
+endif( NOT AVR_UPLOADTOOL )
 
 # default upload tool port
-if(NOT AVR_UPLOADTOOL_PORT)
+if( NOT AVR_UPLOADTOOL_PORT )
    set(
       AVR_UPLOADTOOL_PORT usb
       CACHE STRING "Set default upload tool port: usb"
    )
-endif(NOT AVR_UPLOADTOOL_PORT)
+endif( NOT AVR_UPLOADTOOL_PORT )
 
 # default programmer (hardware)
-if(NOT AVR_PROGRAMMER)
+if( NOT AVR_PROGRAMMER )
    set(
-      AVR_PROGRAMMER avrispmkII
-      CACHE STRING "Set default programmer hardware model: avrispmkII"
+      AVR_PROGRAMMER arduino
+      CACHE STRING "Set default programmer hardware model: arduino"
    )
-endif(NOT AVR_PROGRAMMER)
+endif( NOT AVR_PROGRAMMER )
+
+# default programmer upload speed
+if( NOT AVR_UPLOAD_SPEED )
+   set(
+      AVR_UPLOAD_SPEED 115200
+      CACHE STRING "Set default AVR_UPLOAD_SPEED: 115200 baud"
+   )
+endif( NOT AVR_UPLOAD_SPEED )
 
 # default MCU (chip)
-if(NOT AVR_MCU)
+if( NOT AVR_MCU )
    set(
-      AVR_MCU atmega8
-      CACHE STRING "Set default MCU: atmega8 (see 'avr-gcc --target-help' for valid values)"
+      AVR_MCU atmega328p
+      CACHE STRING "Set default MCU: atmega328p (see 'avr-gcc --target-help' for valid values)"
    )
-endif(NOT AVR_MCU)
+endif( NOT AVR_MCU )
 
-#default avr-size args
-if(NOT AVR_SIZE_ARGS)
-   if(APPLE)
-      set(AVR_SIZE_ARGS -B)
-   else(APPLE)
-      set(AVR_SIZE_ARGS -C;--mcu=${AVR_MCU})
-   endif(APPLE)
-endif(NOT AVR_SIZE_ARGS)
+# default MCU speed
+if( NOT AVR_MCU_SPEED )
+    set(
+       AVR_MCU_SPEED 16000000UL
+       CACHE STRING "Set default MCU SPEED: 16000000UL"
+    )
+endif( NOT AVR_MCU_SPEED )
+
+# default avr-size args
+if( NOT AVR_SIZE_ARGS )
+   if( APPLE )
+      set( AVR_SIZE_ARGS -B )
+   else( APPLE )
+      set( AVR_SIZE_ARGS -C;--mcu=${AVR_MCU} )
+   endif( APPLE )
+endif( NOT AVR_SIZE_ARGS )
+
+# Prep avrdude special options
+if( AVR_UPLOADTOOL MATCHES avrdude )
+    set( AVR_UPLOADTOOL_OPTIONS -b${AVR_UPLOAD_SPEED} -D -V )
+endif( AVR_UPLOADTOOL MATCHES avrdude )
+
+# Set the awk arguments
+set( AWK_ARGS -f firmwaresize.awk )
+
+
+
+##########################################################################
+# set default compiler options:
+##########################################################################
+set( CMAKE_CXX_FLAGS "-fno-exceptions"  CACHE STRING "Default C++ flags for all builds" )
+
+set( CMAKE_C_FLAGS_RELEASE "-O3" CACHE STRING "Default C flags for release" )
+set( CMAKE_CXX_FLAGS_RELEASE "-O3" CACHE STRING "Default C++ flags for release" )
+
+set( CMAKE_C_FLAGS_MINSIZEREL "-Os -mcall-prologues" CACHE STRING "Default C flags for minimum size release" )
+set( CMAKE_CXX_FLAGS_MINSIZEREL "-Os -mcall-prologues" CACHE STRING "Default C++ flags for minimum size release" )
+
+set( CMAKE_C_FLAGS_DEBUG "-g" CACHE STRING "Default C flags for debug" )
+set( CMAKE_CXX_FLAGS_DEBUG "-g" CACHE STRING "Default C++ flags for debug" )
+
+set( CMAKE_C_FLAGS_RELWITHDEBINFO "-O3 -g" CACHE STRING "Default C flags for release with debug info" )
+set( CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O3 -g" CACHE STRING "Default C++ flags for release with debug info" )
+
+
 
 ##########################################################################
 # check build types:
 # - Debug
 # - Release
+# - MinSizeRel
 # - RelWithDebInfo
 #
 # Release is chosen, because of some optimized functions in the
 # AVR toolchain, e.g. _delay_ms().
 ##########################################################################
-if(NOT ((CMAKE_BUILD_TYPE MATCHES Release) OR
+if( NOT ( (CMAKE_BUILD_TYPE MATCHES Release) OR
         (CMAKE_BUILD_TYPE MATCHES RelWithDebInfo) OR
         (CMAKE_BUILD_TYPE MATCHES Debug) OR
-        (CMAKE_BUILD_TYPE MATCHES MinSizeRel)))
+        (CMAKE_BUILD_TYPE MATCHES MinSizeRel) ) )
    set(
       CMAKE_BUILD_TYPE Release
       CACHE STRING "Choose cmake build type: Debug Release RelWithDebInfo MinSizeRel"
       FORCE
    )
-endif(NOT ((CMAKE_BUILD_TYPE MATCHES Release) OR
+endif( NOT ( (CMAKE_BUILD_TYPE MATCHES Release) OR
            (CMAKE_BUILD_TYPE MATCHES RelWithDebInfo) OR
            (CMAKE_BUILD_TYPE MATCHES Debug) OR
-           (CMAKE_BUILD_TYPE MATCHES MinSizeRel)))
+           (CMAKE_BUILD_TYPE MATCHES MinSizeRel) ) )
 
-##########################################################################
+
+
 
 ##########################################################################
 # target file name add-on
 ##########################################################################
-if(WITH_MCU)
-   set(MCU_TYPE_FOR_FILENAME "-${AVR_MCU}")
-else(WITH_MCU)
-   set(MCU_TYPE_FOR_FILENAME "")
-endif(WITH_MCU)
+if( WITH_MCU )
+   set( MCU_TYPE_FOR_FILENAME "-${AVR_MCU}" )
+else( WITH_MCU )
+   set( MCU_TYPE_FOR_FILENAME "" )
+endif( WITH_MCU )
+
+
+##########################################################################
+# status messages
+##########################################################################
+message( STATUS "Current uploadtool is: ${AVR_UPLOADTOOL}" )
+message( STATUS "Current programmer is: ${AVR_PROGRAMMER}" )
+message( STATUS "Current upload port is: ${AVR_UPLOADTOOL_PORT}" )
+message( STATUS "Current uploadtool options are: ${AVR_UPLOADTOOL_OPTIONS}" )
+message( STATUS "Current AVR MCU is set to: ${AVR_MCU}" )
+message( STATUS "Current AVR MCU speed is set to: ${AVR_MCU_SPEED}" )
+
+
+
 
 ##########################################################################
 # add_avr_executable
@@ -145,34 +239,45 @@ endif(WITH_MCU)
 # any link dependencies need to be using that target, e.g. for
 # target_link_libraries(<EXECUTABLE_NAME>-${AVR_MCU}.elf ...).
 ##########################################################################
-function(add_avr_executable EXECUTABLE_NAME)
-   if(NOT ARGN)
-      message(FATAL_ERROR "No source files given for ${EXECUTABLE_NAME}.")
-   endif(NOT ARGN)
+
+function( add_avr_executable EXECUTABLE_NAME )
+
+   if( NOT ARGN )
+      message( FATAL_ERROR "No source files given for ${EXECUTABLE_NAME}." )
+   endif( NOT ARGN )
 
    # set file names
-   set(elf_file ${EXECUTABLE_NAME}${MCU_TYPE_FOR_FILENAME}.elf)
-   set(hex_file ${EXECUTABLE_NAME}${MCU_TYPE_FOR_FILENAME}.hex)
-   set(map_file ${EXECUTABLE_NAME}${MCU_TYPE_FOR_FILENAME}.map)
-   set(eeprom_image ${EXECUTABLE_NAME}${MCU_TYPE_FOR_FILENAME}-eeprom.hex)
+   set( elf_file ${EXECUTABLE_NAME}${MCU_TYPE_FOR_FILENAME}.elf )
+   set( hex_file ${EXECUTABLE_NAME}${MCU_TYPE_FOR_FILENAME}.hex )
+   set( map_file ${EXECUTABLE_NAME}${MCU_TYPE_FOR_FILENAME}.map )
+   set( eeprom_image ${EXECUTABLE_NAME}${MCU_TYPE_FOR_FILENAME}-eeprom.hex )
 
    # elf file
-   add_executable(${elf_file} EXCLUDE_FROM_ALL ${ARGN})
+   add_executable( ${elf_file} EXCLUDE_FROM_ALL ${ARGN} )
 
-   set_target_properties(
-      ${elf_file}
-      PROPERTIES
-         COMPILE_FLAGS "-mmcu=${AVR_MCU}"
-         LINK_FLAGS "-mmcu=${AVR_MCU} -Wl,--gc-sections -mrelax -Wl,-Map,${map_file}"
+   target_compile_options( 
+        ${elf_file} PUBLIC
+        -ffunction-sections 
+        -fdata-sections 
+        -fpack-struct 
+        -fshort-enums 
+        -funsigned-char 
+        -funsigned-bitfields
+        -mmcu=${AVR_MCU} 
+        -DF_CPU=${AVR_MCU_SPEED}
    )
+   
+   target_link_libraries( 
+        ${elf_file} "-mmcu=${AVR_MCU} -Wl,--gc-sections -mrelax -Wl,-Map,${map_file}" 
+    )
 
    add_custom_command(
       OUTPUT ${hex_file}
       COMMAND
          ${AVR_OBJCOPY} -j .text -j .data -O ihex ${elf_file} ${hex_file}
       COMMAND
-         ${AVR_SIZE_TOOL} ${AVR_SIZE_ARGS} ${elf_file}
-      DEPENDS ${elf_file}
+         ${AVR_SIZE_TOOL} ${AVR_SIZE_ARGS} ${elf_file} "|" ${AWK} ${AWK_ARGS}
+      DEPENDS ${elf_file} firmwaresize.awk
    )
 
    # eeprom
@@ -198,7 +303,7 @@ function(add_avr_executable EXECUTABLE_NAME)
    )
 
    # clean
-   get_directory_property(clean_files ADDITIONAL_MAKE_CLEAN_FILES)
+   get_directory_property( clean_files ADDITIONAL_MAKE_CLEAN_FILES )
    set_directory_properties(
       PROPERTIES
          ADDITIONAL_MAKE_CLEAN_FILES "${map_file}"
@@ -232,40 +337,6 @@ function(add_avr_executable EXECUTABLE_NAME)
       COMMENT "Get status from ${AVR_MCU}"
    )
 
-   # get fuses
-   add_custom_target(
-      get_fuses
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT} -n
-         -U lfuse:r:-:b
-         -U hfuse:r:-:b
-      COMMENT "Get fuses from ${AVR_MCU}"
-   )
-
-   # set fuses
-   add_custom_target(
-      set_fuses
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT}
-         -U lfuse:w:${AVR_L_FUSE}:m
-         -U hfuse:w:${AVR_H_FUSE}:m
-         COMMENT "Setup: High Fuse: ${AVR_H_FUSE} Low Fuse: ${AVR_L_FUSE}"
-   )
-
-   # get oscillator calibration
-   add_custom_target(
-      get_calibration
-         ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT}
-         -U calibration:r:${AVR_MCU}_calib.tmp:r
-         COMMENT "Write calibration status of internal oscillator to ${AVR_MCU}_calib.tmp."
-   )
-
-   # set oscillator calibration
-   add_custom_target(
-      set_calibration
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT}
-         -U calibration:w:${AVR_MCU}_calib.hex
-         COMMENT "Program calibration status of internal oscillator from ${AVR_MCU}_calib.hex."
-   )
-
    # disassemble
    add_custom_target(
       disassemble_${EXECUTABLE_NAME}
@@ -273,7 +344,33 @@ function(add_avr_executable EXECUTABLE_NAME)
       DEPENDS ${elf_file}
    )
 
-endfunction(add_avr_executable)
+   # size
+   add_custom_target(
+      size_${EXECUTABLE_NAME}
+         ${AVR_SIZE_TOOL} ${AVR_SIZE_ARGS} ${elf_file}  "|" ${AWK} ${AWK_ARGS}
+      DEPENDS ${elf_file} firmwaresize.awk
+   )
+
+   add_custom_target(
+      firmwaresize.awk
+      COMMAND
+        echo "BEGIN {ORS=\"\";print \"\\\\n\\\\033[1;33mFirmware size (\"}" > firmwaresize.awk
+      COMMAND
+        echo "/^Device/ {print \$2 \") is...  \"}" >> firmwaresize.awk
+      COMMAND
+        echo "/^Program/ {print \"Flash (program): \" \$2 \" \" \$3 \" \" \$4 \")  \"}" >> firmwaresize.awk 
+      COMMAND
+        echo "/^Data/ {print \"RAM\ (globals): \" \$2 \" \" \$3 \" \" \$4 \")  \"}" >> firmwaresize.awk
+      COMMAND
+        echo "END {print \"\\\\033[0m\\\\n\\\\n\"}" >> firmwaresize.awk
+      VERBATIM
+   )
+
+endfunction( add_avr_executable )
+
+
+
+
 
 ##########################################################################
 # add_avr_library
@@ -284,23 +381,35 @@ endfunction(add_avr_executable)
 # This needs to be used for linking against the library, e.g. calling
 # target_link_libraries(...).
 ##########################################################################
-function(add_avr_library LIBRARY_NAME)
-   if(NOT ARGN)
-      message(FATAL_ERROR "No source files given for ${LIBRARY_NAME}.")
-   endif(NOT ARGN)
 
-   set(lib_file ${LIBRARY_NAME}${MCU_TYPE_FOR_FILENAME})
+function( add_avr_library LIBRARY_NAME )
 
-   add_library(${lib_file} STATIC ${ARGN})
+   if( NOT ARGN )
+      message( FATAL_ERROR "No source files given for ${LIBRARY_NAME}." )
+   endif( NOT ARGN )
+
+   set( lib_file ${LIBRARY_NAME}${MCU_TYPE_FOR_FILENAME} )
+
+   add_library( ${lib_file} STATIC ${ARGN} )
 
    set_target_properties(
       ${lib_file}
       PROPERTIES
-         COMPILE_FLAGS "-mmcu=${AVR_MCU}"
          OUTPUT_NAME "${lib_file}"
    )
 
-   if(NOT TARGET ${LIBRARY_NAME})
+   target_compile_options( ${lib_file} PUBLIC
+        -ffunction-sections 
+        -fdata-sections 
+        -fpack-struct 
+        -fshort-enums 
+        -funsigned-char 
+        -funsigned-bitfields
+        -mmcu=${AVR_MCU} 
+        -DF_CPU=${AVR_MCU_SPEED}
+   )
+
+   if( NOT TARGET ${LIBRARY_NAME} )
       add_custom_target(
          ${LIBRARY_NAME}
          ALL
@@ -312,9 +421,15 @@ function(add_avr_library LIBRARY_NAME)
          PROPERTIES
             OUTPUT_NAME "${lib_file}"
       )
-   endif(NOT TARGET ${LIBRARY_NAME})
+   endif( NOT TARGET ${LIBRARY_NAME} )
 
-endfunction(add_avr_library)
+endfunction( add_avr_library )
+
+
+
+
+
+
 
 ##########################################################################
 # avr_target_link_libraries
@@ -324,21 +439,24 @@ endfunction(add_avr_library)
 # Calls target_link_libraries with AVR target names (concatenation,
 # extensions and so on.
 ##########################################################################
-function(avr_target_link_libraries EXECUTABLE_TARGET)
-   if(NOT ARGN)
-      message(FATAL_ERROR "Nothing to link to ${EXECUTABLE_TARGET}.")
-   endif(NOT ARGN)
 
-   get_target_property(TARGET_LIST ${EXECUTABLE_TARGET} OUTPUT_NAME)
+function( avr_target_link_libraries EXECUTABLE_TARGET )
 
-   foreach(TGT ${ARGN})
-      if(TARGET ${TGT})
-         get_target_property(ARG_NAME ${TGT} OUTPUT_NAME)
-         list(APPEND TARGET_LIST ${ARG_NAME})
-      else(TARGET ${TGT})
-         list(APPEND NON_TARGET_LIST ${TGT})
-      endif(TARGET ${TGT})
-   endforeach(TGT ${ARGN})
+   if( NOT ARGN )
+      message( FATAL_ERROR "Nothing to link to ${EXECUTABLE_TARGET}." )
+   endif( NOT ARGN )
 
-   target_link_libraries(${TARGET_LIST} ${NON_TARGET_LIST})
-endfunction(avr_target_link_libraries EXECUTABLE_TARGET)
+   get_target_property( TARGET_LIST ${EXECUTABLE_TARGET} OUTPUT_NAME )
+
+   foreach( TGT ${ARGN} )
+      if( TARGET ${TGT} )
+         get_target_property( ARG_NAME ${TGT} OUTPUT_NAME )
+         list( APPEND TARGET_LIST ${ARG_NAME} )
+      else( TARGET ${TGT} )
+         list( APPEND NON_TARGET_LIST ${TGT} )
+      endif( TARGET ${TGT} )
+   endforeach( TGT ${ARGN} )
+
+   target_link_libraries( ${TARGET_LIST} ${NON_TARGET_LIST} )
+   
+endfunction( avr_target_link_libraries EXECUTABLE_TARGET )
